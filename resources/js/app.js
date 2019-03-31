ffe2a257aaf29de370ee3e655959737d0a935719
vagrant <vagrant@vuelarastart>
2019-03-31 13:15:11 +0000

    instaled modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,11 +18,13 @@ Vue.use(VueRouter)
 
 let routes = [
   { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+  { path: '/users', component: require('./components/Users.vue').default },
   { path: '/profile', component: require('./components/Profile.vue').default }
   
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes // short for `routes: routes`
 })
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -74,7 +74,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item">
             <router-link to="/dashboard" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-tachometer-alt text-blue"></i>
               <p>
                 Dashboard
                 
@@ -83,45 +83,47 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tasks"></i>
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tasks text-yellow"></i>
               <p>
                 Management
-                <i class="right fas fa-angle-left"></i>
+                <i class="right fas fa-angle-left text-yellow"></i>
               </p>
             </ra>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
+                <router-link to="/users" class="nav-link">
+                  <i class="fas fa-users nav-icon text-teal"></i>
+                  <p>Users</p>
+                </router-link>
               </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
+
             </ul>
           </li>
             <li class="nav-item">
             <router-link to="/profile" class="nav-link">
-              <i class="nav-icon fas fa-user-alt"></i>
+              <i class="nav-icon fas fa-user-alt text-green"></i>
               <p>
                 Profile
                 
               </p>
             </router-link>
           </li>
+
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-sign-out-alt"></i>
-              <p>
-                Logout
-                
-              </p>
+
+            <a class="nav-link" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+               document.getElementById('logout-form').submit();">
+               <i class="nav-icon fas fa-sign-out-alt text-red"></i>
+               <p>
+                 {{ __('Logout') }}
+               </p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </li>
 
         </ul>
