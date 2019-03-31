16af05afbb4bf6b11052c6525bbace91fe95c408
vagrant <vagrant@vuelarastart>
2019-03-31 15:03:22 +0000

    user form created 13. lection

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,24 +9,30 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import { Form, HasError, AlertError } from 'vform';
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+
+
 /**
  * Router setup
  */
 
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
 let routes = [
   { path: '/dashboard', component: require('./components/Dashboard.vue').default },
   { path: '/users', component: require('./components/Users.vue').default },
   { path: '/profile', component: require('./components/Profile.vue').default }
   
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   routes // short for `routes: routes`
-})
+});
 
 /**
  * The following block of code may be used to automatically register your
