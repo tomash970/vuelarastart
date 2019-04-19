f696198ef1fba7d6367377a81de32b3419dd7b72
vagrant <vagrant@vuelarastart>
2019-04-19 07:09:58 +0000

    finished 48 pagination

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,8 @@ window.Form = Form;
 Vue.component(HasError.name, HasError);
 Vue.component(AlertError.name, AlertError);
 
+Vue.component('pagination', require('laravel-vue-pagination'));
+
 /**
  * fire
  */
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -25,7 +25,7 @@
                     <th>Created at</th>
                     <th>Modify</th>
                   </tr>
-                  <tr v-for="user in users" :key="user.id">
+                  <tr v-for="user in users.data" :key="user.id">
                     <td>{{user.id}}</td>
                     <td>{{user.photo}}</td>
                     <td>{{user.name}}</td>
@@ -48,6 +48,10 @@
                 </table>
               </div>
               <!-- /.card-body -->
+              <div class="card-footer">
+                  <pagination :data="users" @pagination-change-page="getResults"></pagination>
+              </div>
+              <!-- /.card-footer -->
             </div>
             <!-- /.card -->
           </div>
@@ -136,6 +140,12 @@
             }
         },
         methods: {
+            getResults(page = 1) {
+                axios.get('api/user?page=' + page)
+                    .then(response => {
+                        this.users = response.data;
+                    });
+            },
             updateUser(){
                 this.$Progress.start();
                 //console.log('editingdata')
@@ -195,7 +205,7 @@
             },
             loadUsers() {
                 if(this.$gate.isAdminOrAuthor()){
-                    axios.get("api/user").then(({data}) => (this.users = data.data));
+                    axios.get("api/user").then(({data}) => (this.users = data));
                 }
                 
             },
